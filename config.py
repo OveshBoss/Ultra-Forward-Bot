6fa6d1cfc3143e44201cb47fe41ecd0e8a10d286
ovesh boss <oveshboss.08@gmail.com>
2025-11-05 11:47:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "579f1bcf3eac1660d81ef34b09906012")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "8335107980:AAFPidlKhHoFCIZnDHCVjnDci_H5YDHl-8E") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "mongodb+srv://Ovesh:ovesh.boss@ovesh.95jpp8g.mongodb.net/?retryWrites=true&w=majority&appName=Ovesh")
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://hackersehzan_db_user:7XwTVPgxUxmswpmL@cluster0.sxwdosl.mongodb.net/?appName=Cluster0")
     DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
     OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '1416433622').split()]
 
